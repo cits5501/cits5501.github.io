524d7c1fdf883f4991ed74b98feae37ce1bd7663
arran <arranstewart@users.noreply.github.com>
2022-03-16 20:15:03 +0800

    remove old scripts for fetching artifacts, add new one

--- copy_workshops_and_sols.sh
+++
@@ -1,5 +0,0 @@
-
-set -ex
-cp ../cits5501-sources/workshops/workshop[0-9][0-9]/workshop[0-9][0-9].pdf docs/workshops
-cp ../cits5501-sources/workshops/workshop[0-9][0-9]/workshop[0-9][0-9]-solutions.pdf docs/workshops
-
---
+++ fetch_artifacts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+tmpdir=$(mktemp --directory --quiet)
+
+git clone --single-branch --branch built git@github.com:cits5501/cits5501-materials.git $tmpdir
+
+cp -a $tmpdir/lectures/*pdf assets/lectures/
+cp -a $tmpdir/lectures-markdown/*md assets/lectures/
+cp -a $tmpdir/workshops/*.{pdf,md} assets/workshops/
+
+rm -rf $tmpdir
+
