f1c85cab20e3519df5385095b02fae41cb55bdd5
arran <arranstewart@users.noreply.github.com>
2022-03-24 17:47:14 +0800

    update fetch script: copy code .zip files

--- fetch_artifacts.sh
+++ fetch_artifacts.sh
@@ -9,7 +9,7 @@ git clone --single-branch --branch built git@github.com:cits5501/cits5501-materi
 
 cp -a $tmpdir/lectures/*pdf assets/lectures/
 cp -a $tmpdir/lectures-markdown/*md assets/lectures/
-cp -a $tmpdir/workshops/*.{pdf,md} assets/workshops/
+cp -a $tmpdir/workshops/*.{pdf,md,zip} assets/workshops/
 
 rm -rf $tmpdir
 
